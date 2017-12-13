4ad7eb7f50583150a75ba1db3b8a0ecc458fa53c
pentantan <shutux500@gmail.com>
2017-12-14 00:51:04 +0900

    3.3.0

--- src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
+++ src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
@@ -38,7 +38,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
-    public static final String VERSION = "3.2.4";
+    public static final String VERSION = "3.3.0";
     public static final String MODNAME = "JapariCraftMod";
 
 
