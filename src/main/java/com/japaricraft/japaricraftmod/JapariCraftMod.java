6a14ec16688becabd65b247fc6350a8e9041e6eb
pentantan <shutux500@gmail.com>
2017-12-23 21:51:20 +0900

    3.3.3サンドスターロウの浄化レシピ追加

--- src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
+++ src/main/java/com/japaricraft/japaricraftmod/JapariCraftMod.java
@@ -38,7 +38,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
-    public static final String VERSION = "3.3.1";
+    public static final String VERSION = "3.3.3";
     public static final String MODNAME = "JapariCraftMod";
 
 
