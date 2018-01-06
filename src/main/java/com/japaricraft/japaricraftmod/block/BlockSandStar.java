973cb4f4a54abeb007ab58bec16085f907ac64e9
pentantan <shutux500@gmail.com>
2018-01-06 20:13:00 +0900

    爆破耐性変更、連携レシピの追加

--- src/main/java/com/japaricraft/japaricraftmod/block/BlockSandStar.java
+++ src/main/java/com/japaricraft/japaricraftmod/block/BlockSandStar.java
@@ -17,7 +17,7 @@ public class BlockSandStar extends Block {
         setCreativeTab(JapariCraftMod.tabJapariCraft);
         setUnlocalizedName("SandStarBlock");
         setHardness(4.0F);/*硬さ*/
-        setResistance(30.0F);/*爆破耐性*/
+        setResistance(20.0F);/*爆破耐性*/
         setSoundType(SoundType.GLASS);
         setLightLevel(10);
         setHarvestLevel("pickaxe",1);
