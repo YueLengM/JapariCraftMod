d9981707323a9f9e29dad97a7c447db9129ebf51
pentantan <shutux500@gmail.com>
2018-01-25 05:15:13 +0900

    少し変更

--- src/main/java/com/japaricraft/japaricraftmod/CommonProxy.java
+++ src/main/java/com/japaricraft/japaricraftmod/CommonProxy.java
@@ -1,10 +1,8 @@
 package com.japaricraft.japaricraftmod;
 
 
-import com.japaricraft.japaricraftmod.world.MapGenSandStarRuin;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.world.gen.structure.MapGenStructure;
 
 public class CommonProxy{
 
@@ -17,9 +15,4 @@ public class CommonProxy{
     public void registerFluidBlockRendering(Block fluidBlock, String name) {
     }
 
-    public MapGenSandStarRuin chest = new MapGenSandStarRuin();
-
-    public MapGenStructure getTreasureMapGen() {
-        return chest;
-    }
 }
