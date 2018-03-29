1fceecfdbb3051a9f4cd277ff7f37653ab51e0b8
pentantan <shutux500@gmail.com>
2018-03-29 11:16:46 +0900

    fix crash armor error in ServerSide

--- src/main/java/com/japaricraft/japaricraftmod/gui/ContainerFriendInventory.java
+++ src/main/java/com/japaricraft/japaricraftmod/gui/ContainerFriendInventory.java
@@ -8,20 +8,17 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
 import java.util.Set;
 
-public class ContainerFriendInventory extends Container
-{
+public class ContainerFriendInventory extends Container {
     private static final Set<Item> Neck_ITEMS = Sets.newHashSet(JapariItems.sandstarnecklace);
     private EntityFriend entityFriend;
     private EntityPlayer entityPlayer;
 
 
-    public ContainerFriendInventory(EntityFriend entityFriend, EntityPlayer entityPlayer)
-    {
+    public ContainerFriendInventory(EntityFriend entityFriend, EntityPlayer entityPlayer) {
         int column;
         int row;
         int index;
@@ -30,71 +27,50 @@ public class ContainerFriendInventory extends Container
         entityFriend.getInventoryFriendEquipment().openInventory(entityPlayer);
 
         //フレンズの装備スロットを追加する
-        for (index = 0; index < 3; ++index)
-        {
-            switch (index)
-            {
-                /*0は胸の装備スロット
-                1は足、2は頭
-                 */
-                case 0 :
-                    this.addSlotToContainer(new Slot(entityFriend.getInventoryFriendEquipment(), index, 8, 18)
-                    {
-                        public int getSlotStackLimit() {
-                            return 1;
-                        }
-                        @Override
-                        public boolean isItemValid(ItemStack stack)
-                        {
-                            if (stack.isEmpty()) return false;
-                            boolean flag = !stack.isEmpty() && stack.getItem() instanceof ItemArmor &&
-                                    ((ItemArmor) stack.getItem()).getEquipmentSlot() == EntityEquipmentSlot.CHEST;
-                            return flag;
+        for (index = 0; index < 3; ++index) {
 
-                        }
-                    });
+            final EntityEquipmentSlot slotType;
+            int x = 0, y = 0;
+            switch (index) {
+                case 0:
+                    slotType = EntityEquipmentSlot.CHEST;
+                    x = 8;
+                    y = 18;
                     break;
-                case 1 :
-                    this.addSlotToContainer(new Slot(entityFriend.getInventoryFriendEquipment(), index, 8, 36) {
-
-                        public int getSlotStackLimit() {
-                            return 1;
-                        }
-                        @Override
-                        public boolean isItemValid(ItemStack stack) {
-                            if (stack.isEmpty()) return false;
-                            boolean flag = !stack.isEmpty() && stack.getItem() instanceof ItemArmor &&
-                                    ((ItemArmor) stack.getItem()).getEquipmentSlot() == EntityEquipmentSlot.FEET;
-                            return flag;
-
-                        }
-                    });
+                case 1:
+                    slotType = EntityEquipmentSlot.FEET;
+                    x = 8;
+                    y = 36;
                     break;
                 case 2:
-                    this.addSlotToContainer(new Slot(entityFriend.getInventoryFriendEquipment(), index, 80, 36) {
-                        public int getSlotStackLimit() {
-                            return 1;
-                        }
-                        @Override
-                        public boolean isItemValid(ItemStack stack) {
-                            if (stack.isEmpty()) return false;
-                            boolean flag = !stack.isEmpty() && stack.getItem() instanceof ItemArmor &&
-                                    ((ItemArmor) stack.getItem()).getEquipmentSlot() == EntityEquipmentSlot.HEAD;
-                            boolean flag2 = !stack.isEmpty() && Neck_ITEMS.contains(stack.getItem());
-                            return flag || flag2;
-
-                        }
-                    });
+                    slotType = EntityEquipmentSlot.HEAD;
+                    x = 80;
+                    y = 36;
                     break;
+                default:
+                    slotType = null;
             }
+            this.addSlotToContainer(new Slot(entityFriend.getInventoryFriendEquipment(), index, x, y) {
+                public int getSlotStackLimit() {
+                    return 1;
+                }
+
+                @Override
+                public boolean isItemValid(ItemStack stack) {
+                    if (slotType == null)
+                        return false;
+                    if (stack.getItem().isValidArmor(stack, slotType, entityFriend)) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
         }
 
         entityFriend.getInventoryFriendMain().openInventory(entityPlayer);
 
-        for (column = 0; column < 3; ++column)
-        {
-            for (row = 0; row < 9; ++row)
-            {
+        for (column = 0; column < 3; ++column) {
+            for (row = 0; row < 9; ++row) {
                 index = (row + column * 9);
 
                 this.addSlotToContainer(new Slot(entityFriend.getInventoryFriendMain(), index, (row * 18) + 8, (column * 18) + 74));
@@ -103,18 +79,15 @@ public class ContainerFriendInventory extends Container
 
         entityPlayer.inventory.openInventory(entityPlayer);
 
-        for (column = 0; column < 3; ++column)
-        {
-            for (row = 0; row < 9; ++row)
-            {
+        for (column = 0; column < 3; ++column) {
+            for (row = 0; row < 9; ++row) {
                 index = (row + column * 9 + 9);
 
                 this.addSlotToContainer(new Slot(entityPlayer.inventory, index, (row * 18) + 8, (column * 18) + 140));
             }
         }
 
-        for (row = 0; row < 9; ++row)
-        {
+        for (row = 0; row < 9; ++row) {
             index = row;
 
             this.addSlotToContainer(new Slot(entityPlayer.inventory, index, (row * 18) + 8, 198));
@@ -125,19 +98,16 @@ public class ContainerFriendInventory extends Container
     }
 
     @Override
-    public boolean canInteractWith(EntityPlayer playerIn)
-    {
+    public boolean canInteractWith(EntityPlayer playerIn) {
         return this.entityFriend.getInventoryFriendMain().isUsableByPlayer(playerIn);
     }
 
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer player, int index)
-    {
+    public ItemStack transferStackInSlot(EntityPlayer player, int index) {
         ItemStack stackEmpty = ItemStack.EMPTY;
         Slot slot = this.inventorySlots.get(index);
 
-        if (slot == null || !slot.getHasStack())
-        {
+        if (slot == null || !slot.getHasStack()) {
             return stackEmpty;
         }
 
@@ -149,32 +119,23 @@ public class ContainerFriendInventory extends Container
         // int indexEquipment = 5;
         // int indexMain = 27;
 
-        if (index < 32)
-        {
-            if (!this.mergeItemStack(dstItemStack, 32, this.inventorySlots.size(), true))
-            {
+        if (index < 32) {
+            if (!this.mergeItemStack(dstItemStack, 32, this.inventorySlots.size(), true)) {
                 return stackEmpty;
             }
-        }
-        else
-        {
-            if (!this.mergeItemStack(dstItemStack, 3, 32, false))
-            {
+        } else {
+            if (!this.mergeItemStack(dstItemStack, 3, 32, false)) {
                 return stackEmpty;
             }
         }
 
-        if (dstItemStack.isEmpty())
-        {
+        if (dstItemStack.isEmpty()) {
             slot.putStack(stackEmpty);
-        }
-        else
-        {
+        } else {
             slot.onSlotChanged();
         }
 
-        if (dstItemStack.getCount() == srcItemStack.getCount())
-        {
+        if (dstItemStack.getCount() == srcItemStack.getCount()) {
             return stackEmpty;
         }
 
@@ -184,8 +145,7 @@ public class ContainerFriendInventory extends Container
     }
 
     @Override
-    public void onContainerClosed(EntityPlayer playerIn)
-    {
+    public void onContainerClosed(EntityPlayer playerIn) {
         this.entityFriend.getInventoryFriendMain().closeInventory(playerIn);
         this.entityFriend.getInventoryFriendEquipment().closeInventory(playerIn);
         this.entityPlayer.inventory.openInventory(playerIn);
