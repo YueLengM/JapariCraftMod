cbd3c8f36d78f56ce96a5b36dc5ddd27914181e8
pentantan <shutux500@gmail.com>
2018-07-03 00:21:46 +0900

    change sandstarore weight

--- src/main/java/com/japaricraft/japaricraftmod/world/SandStarOreGenerator.java
+++ src/main/java/com/japaricraft/japaricraftmod/world/SandStarOreGenerator.java
@@ -22,13 +22,12 @@ public class SandStarOreGenerator implements IWorldGenerator {
     private void generateOre(World world, Random random, int x, int z) {
         //1チャンクで生成したい回数だけ繰り返す。
         if (world.provider instanceof WorldProviderSurface) {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 2; i++) {
                 int genX = x + random.nextInt(16);
                 int genY = 1 + random.nextInt(32);
                 int genZ = z + random.nextInt(16);
-                new WorldGenMinable(random.nextInt(100) == 0 ?
-                        JapariBlocks.SANDSTAR_ORE.getDefaultState() :
-                        JapariBlocks.SANDSTAR_ORE.getDefaultState(), 6).generate(world, random, new BlockPos(genX, genY, genZ));
+                new WorldGenMinable(
+                        JapariBlocks.SANDSTAR_ORE.getDefaultState(), 3 + random.nextInt(4)).generate(world, random, new BlockPos(genX, genY, genZ));
             }
         }
     }
