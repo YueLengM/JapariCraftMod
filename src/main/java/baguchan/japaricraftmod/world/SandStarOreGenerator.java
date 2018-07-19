6bd145c6000845e285afc57ec5f424c816dd06f7
pentantan <shutux500@gmail.com>
2018-07-19 19:55:19 +0900

    change OreGenerator

--- src/main/java/baguchan/japaricraftmod/world/SandStarOreGenerator.java
+++ src/main/java/baguchan/japaricraftmod/world/SandStarOreGenerator.java
@@ -22,12 +22,12 @@ public class SandStarOreGenerator implements IWorldGenerator {
     private void generateOre(World world, Random random, int x, int z) {
         //1チャンクで生成したい回数だけ繰り返す。
         if (world.provider instanceof WorldProviderSurface) {
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 3; i++) {
                 int genX = x + random.nextInt(16);
-                int genY = 1 + random.nextInt(32);
+                int genY = 1 + random.nextInt(36);
                 int genZ = z + random.nextInt(16);
                 new WorldGenMinable(
-                        JapariBlocks.SANDSTAR_ORE.getDefaultState(), 3 + random.nextInt(4)).generate(world, random, new BlockPos(genX, genY, genZ));
+                        JapariBlocks.SANDSTAR_ORE.getDefaultState(), 2 + random.nextInt(4)).generate(world, random, new BlockPos(genX, genY, genZ));
             }
         }
     }
