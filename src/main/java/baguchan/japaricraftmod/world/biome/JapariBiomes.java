46931d82fa7c7b88b76880950e25bf8566174a47
pentantan <shutux500@gmail.com>
2019-01-27 08:41:42 +0900

    removed the biome that damages the landscape

--- src/main/java/baguchan/japaricraftmod/world/biome/JapariBiomes.java
+++ src/main/java/baguchan/japaricraftmod/world/biome/JapariBiomes.java
@@ -1,21 +1,14 @@
 package baguchan.japaricraftmod.world.biome;
 
 import net.minecraft.world.biome.Biome;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class JapariBiomes {
-    public static final BiomeSandStarJungle SAND_STAR_JUNGLE = new BiomeSandStarJungle();
 
     public static void registerBiomes(IForgeRegistry<Biome> registry) {
-        registry.register(SAND_STAR_JUNGLE.setRegistryName("sandstar_jungle"));
     }
 
     public static void registerBiomeTypes() {
-        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(SAND_STAR_JUNGLE, 5));
-        BiomeManager.addSpawnBiome(SAND_STAR_JUNGLE);
-        BiomeManager.addVillageBiome(SAND_STAR_JUNGLE, true);
-        BiomeDictionary.addTypes(SAND_STAR_JUNGLE, BiomeDictionary.Type.HOT, BiomeDictionary.Type.WET, BiomeDictionary.Type.JUNGLE, BiomeDictionary.Type.MAGICAL);
+
     }
 }
