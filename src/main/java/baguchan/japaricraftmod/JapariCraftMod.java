5a4106b181bad86e0d8e50bf910500be4a6b4a7e
pentantan <shutux500@gmail.com>
2019-01-26 20:18:31 +0900

    5.2.1

--- src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
+++ src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
@@ -39,7 +39,7 @@ import org.apache.logging.log4j.Logger;
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
-    public static final String VERSION = "5.2.0";
+    public static final String VERSION = "5.2.1";
     public static final String MODNAME = "JapariCraftMod";
 
 
