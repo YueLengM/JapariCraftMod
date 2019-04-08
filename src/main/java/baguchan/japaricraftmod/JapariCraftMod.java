2325f5225767bb6f250eff2abdff90a91b847b99
pentantan <shutux500@gmail.com>
2019-04-08 10:31:29 +0900

    add Limit

--- src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
+++ src/main/java/baguchan/japaricraftmod/JapariCraftMod.java
@@ -45,7 +45,7 @@ import org.apache.logging.log4j.Logger;
 public class JapariCraftMod {
 
     public static final String MODID = "japaricraftmod";
-    public static final String VERSION = "5.3.5";
+    public static final String VERSION = "5.3.6";
     public static final String MODNAME = "JapariCraftMod";
 
 
